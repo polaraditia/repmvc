560ebe02b5ff649fc77928e4aa0c33040323c65a
polaraditia <koobface.v2@gmail.com>
2019-04-23 10:35:14 +0700

    movies

---
+++ HelloAmsMVC/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelloAmsMVC.Models;
+
+namespace HelloAmsMVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Everest" };
+            return View(movie);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HelloAmsMVC/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloAmsMVC.Models
+{
+    public class Movie
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+
+    }
+}
